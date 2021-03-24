e557a024514ad72b3ebcf57bfadddf84464a8dc8
aLiNaUsHaKoVa <61658563+aLiNaUsHaKoVa@users.noreply.github.com>
2021-03-25 01:01:30 +0500

    Reformart TrajectoryPoint

--- TrajectoryWithResistance/Trajectory/TrajectoryPoint.cs
+++ TrajectoryWithResistance/Trajectory/TrajectoryPoint.cs
@@ -4,14 +4,15 @@ namespace Trajectory
 {
     public class TrajectoryPoint
     {
+        public float TimeInSeconds { get; }
+
+        public PointF Coords { get; }
+
         public TrajectoryPoint(float timeInSeconds, PointF coords)
         {
             TimeInSeconds = timeInSeconds;
             Coords = coords;
         }
-
-        public float TimeInSeconds { get; }
-        public PointF Coords { get; }
 
         public override string ToString()
         {
